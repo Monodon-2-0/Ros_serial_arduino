8d0b5c67502bdb4b8cecc91aaadd59d57e9198c7
germanmonodon <german@monodon.es>
2024-01-02 13:31:17 +0100

    changed to work rith rp arduino

--- src/serial_comms/scripts/talker.py
+++ src/serial_comms/scripts/talker.py
@@ -37,20 +37,37 @@
 ## to the 'chatter' topic
 
 import rospy
+import socket
+
 from std_msgs.msg import String
 
-def talker():
-    pub = rospy.Publisher('toggle_led', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "Yes"
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+
+class Communicator:
+    def __init__(self):
+        self.pub = rospy.Publisher('toggle_led', String, queue_size=10)
+        rospy.init_node('talker', anonymous=True)
+        self.receiver()
+
+    def receiver(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    print(f"Data received {data}")
+                    if not data:
+                        break
+                    conn.sendall(data)
+                    self.pub.publish(data.decode('utf-8'))
 
 if __name__ == '__main__':
     try:
-        talker()
+        Communicator()
     except rospy.ROSInterruptException:
         pass
